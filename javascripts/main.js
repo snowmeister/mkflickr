440445a446ca6ea228281675e6931a715fd61eb5
Mark Kennard AKA Snowmeister <mark.kennard@gmail.com>
2013-06-09 19:25:05 +0100

    Added basic demo to GH page

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,13 @@
-console.log('This would be the main JS file.');
+$(document).ready(function () {
+	console.log('we are calling it..')
+	$("#gallery-test").mkflickr({
+		// NOTE - Change this to YOUR Flickr Developers API Key
+		apiKey: 'd980cbf534d97b23fa2664329cc40f98',
+		// NOTE - Unless you WANT to see my images, change this to YOUR desired Flickr users id.
+		userId: '44290892@N08',
+		// Set how many images you want to display...
+		itemCount: '10',
+		// set the tag you'd like to wrap each image in eg: 'span', 'div', 'li'
+		tag: 'span'
+	});
+});
