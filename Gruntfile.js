5194accec1ac9a0f255f04bff8f2414d82ddb80a
Mark Kennard <mark.kennard@enabledware.com>
2013-06-10 17:54:12 +0100

    Added npm install devDependencies to package.json, updated gruntfile for grunt-jsdoc to start testing out jsdoc based documentation techniques as part of this project

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,22 +1,28 @@
-module.exports = function (grunt) {
+module.exports = function(grunt) {
 
   // Project configuration.
   grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON("package.json"),
     // Lint definitions
     jshint: {
       files: ["src/js/<%= pkg.name %>.js"],
       options: {
-        jshintrc: 'node_modules/grunt-contrib-jshint/.jshintrc'
+        jshintrc: "node_modules/grunt-contrib-jshint/.jshintrc"
       }
+    },
+    jsdoc : {
+        dist : {
+            src: ['src/js/jquery.mkflickr.js'], 
+            dest: 'build/doc'
+        }
     },
     uglify: {
       options: {
-        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+        banner: "/*! <%= pkg.name %> <%= grunt.template.today('yyyy-mm-dd') %> */\n"
       },
       build: {
-        src: 'src/js/<%= pkg.name %>.js',
-        dest: 'build/js/<%= pkg.name %>.min.js'
+        src: "src/js/<%= pkg.name %>.js",
+        dest: "build/js/<%= pkg.name %>.min.js"
       }
     },
     csslint: {
@@ -24,34 +30,34 @@ module.exports = function (grunt) {
         options: {
           import: 2
         },
-        src: ['src/css/*.css']
+        src: ["src/css/*.css"]
       },
       lax: {
         options: {
           import: false
         },
-        src: ['src/css/*.css']
+        src: ["src/css/*.css"]
       }
     },
     cssmin: {
       minify: {
         expand: true,
-        cwd: 'src/css/',
-        src: ['*.css', '!*.min.css'],
-        dest: 'build/css/',
-        ext: '.mkflickr.min.css'
+        cwd: "src/css/",
+        src: ["*.css", "!*.min.css"],
+        dest: "build/css/",
+        ext: ".mkflickr.min.css"
       }
     }
   });
 
   // Load the plugin that provides the "uglify" task.
-  grunt.loadNpmTasks('grunt-contrib-csslint');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks("grunt-contrib-csslint");
+  grunt.loadNpmTasks("grunt-contrib-cssmin");
   grunt.loadNpmTasks("grunt-contrib-jshint");
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks("grunt-contrib-uglify");
+  grunt.loadNpmTasks('grunt-jsdoc');
   //grunt.loadNpmTasks("grunt-contrib-concat")
-
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'uglify', 'csslint', 'cssmin']);
+  grunt.registerTask("default", ["jshint", "uglify", "csslint", "cssmin"]);
 
 };
--- src/js/demo.js
+++ src/js/demo.js
@@ -1,13 +1,13 @@
 $(document).ready(function () {
 	console.log('we are calling it..')
 	$("#gallery-test").mkflickr({
-		// NOTE - Change this to YOUR Flickr Developers API Key
+		/** NOTE - Change this to YOUR Flickr Developers API Key */
 		apiKey: 'd980cbf534d97b23fa2664329cc40f98',
-		// NOTE - Unless you WANT to see my images, change this to YOUR desired Flickr users id.
+		/** NOTE - Unless you WANT to see my images, change this to YOUR desired Flickr users id.*/
 		userId: '44290892@N08',
-		// Set how many images you want to display...
+		/** Set how many images you want to display...*/
 		itemCount: '10',
-		// set the tag you'd like to wrap each image in eg: 'span', 'div', 'li'
+		/** set the tag you'd like to wrap each image in eg: 'span', 'div', 'li'*/
 		tag: 'span'
 	});
 });
